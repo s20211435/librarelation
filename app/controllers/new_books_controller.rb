5dca9c8bd0d3ac07dbe154028b4044b54c6c97a6
3401 <isha21107@ict.shimanet.ed.jp>
2023-10-24 10:18:34 +0900

    新しいコミット

--- app/controllers/new_books_controller.rb
+++ app/controllers/new_books_controller.rb
@@ -68,6 +68,10 @@ class NewBooksController < ApplicationController
 
   end
 
+  def isbn_search_process
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_new_book

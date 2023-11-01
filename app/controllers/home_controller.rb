c72bb505e832269ff36a85d9117155e8789e5618
3401 <isha21107@ict.shimanet.ed.jp>
2023-11-01 11:26:02 +0900

    detailrepair

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     all_new_books = NewBook.all
     random_id_array = all_new_books.pluck(:id).shuffle[0]
     @random_new_books = all_new_books.where(id: random_id_array)
+
+    
   end
 end

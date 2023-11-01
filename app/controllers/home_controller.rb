9c81bfef4fddfaa4672e3acaf84d36a6f3804742
3433 <3433@example.com>
2023-11-01 11:26:36 +0900

    aaa

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,10 @@ class HomeController < ApplicationController
     all_new_books = NewBook.all
     random_id_array = all_new_books.pluck(:id).shuffle[0]
     @random_new_books = all_new_books.where(id: random_id_array)
+
+    all_rankings = Ranking.all
+    @first_rankings = Ranking.where(book_rank: '1')
+    @second_rankings = Ranking.where(book_rank: '2')
+    @third_rankings = Ranking.where(book_rank: '3')
   end
 end

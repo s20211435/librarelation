b17f5fd5ae0d7903c8d773ae958a3f1cddc3cb47
2419 <s20211420@pc3.in.izusyo-ch.ed.jp>
2023-11-01 11:29:40 +0900

    更新

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,13 @@ class HomeController < ApplicationController
     all_new_books = NewBook.all
     random_id_array = all_new_books.pluck(:id).shuffle[0]
     @random_new_books = all_new_books.where(id: random_id_array)
+
+    all_recommend_books = RecommendBook.all
+    random_id_array = all_recommend_books.pluck(:id).shuffle[0]
+    @random_recommend_books = all_recommend_books.where(id: random_id_array)
+    @recommend_books = all_recommend_books.
   end
+  
+  
+  
 end
